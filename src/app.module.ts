0fb254baffd66d2aae6de38e1789c32c7afce53c
Sergio Barreras <sergiobg.isc@gmail.com>
2025-07-06 01:19:03 -0700

    Imports BranchesModule into AppModule
    Imports the BranchesModule into the AppModule to make its
    functionality available throughout the application.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { DatabaseModule } from './database/database.module';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { BranchesModule } from './branches/branches.module';
 
 @Module({
-  imports: [DatabaseModule, RestaurantsModule],
+  imports: [DatabaseModule, RestaurantsModule, BranchesModule],
   controllers: [],
   providers: [],
 })
