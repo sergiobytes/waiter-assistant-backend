8234fc9c24da367832bfe8f6dbe7080ed47865f4
Sergio Barreras <sergiobg.isc@gmail.com>
2025-08-18 15:42:56 -0700

    fix(main): update global prefix from 'api' to 'v1'

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('v1');
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
