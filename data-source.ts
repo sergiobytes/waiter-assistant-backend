8c4c3ddfef323e1bea52b544dea6c2c2cc2f0672
Sergio Barreras <sergiobg.isc@gmail.com>
2025-08-18 15:02:50 -0700

    fix(data-source): streamline dotenv import and adjust SSL configuration fix(package): add production migration run script fix(tsconfig): correct structure and include/exclude settings

--- data-source.ts
+++ data-source.ts
@@ -1,21 +1,24 @@
-import * as dotenv from 'dotenv';
+import 'dotenv/config';
 import { DataSource } from 'typeorm';
+import { join } from 'path';
 
-dotenv.config();
+const isProd = process.env.NODE_ENV === 'production';
+const ext = isProd ? 'js' : 'ts';
 
-export const AppDataSoruce = new DataSource({
+export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: process.env.NODE_ENV === 'production',
-  extra:
-    process.env.NODE_ENV === 'production'
-      ? { ssl: { rejectUnauthorized: false } }
-      : {},
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/database/migrations/*.ts'],
+
+  // SSL en producción (ajústalo si tu proveedor exige otra config)
+  ssl: isProd ? { rejectUnauthorized: false } : false,
+
   synchronize: false,
+
+  // OJO: desde dist/, __dirname ya apunta a /dist
+  entities: [join(__dirname, `**/*.entity.${ext}`)],
+  migrations: [join(__dirname, `database/migrations/*.${ext}`)],
 });
